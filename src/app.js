2c9508f285c6af8b16a6c9b7c5f3402c35c01beb
Sushil kumar <94502462+sushilmsg@users.noreply.github.com>
2023-04-17 02:07:51 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,20 +1,29 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 5000;
+var http = require('http');
+//create a server object:
+http.createServer(function (req, res) {
+  res.write('Hello World!'); //write a response to the client
+  res.end(); //end the response
+}).listen(8080); //the server object listens on port 8080
 
-app.use(express.json());
-//require("../src/database/connection.js");
+
+
+// const express = require('express');
+// const app = express();
+// const port = process.env.PORT || 5000;
+
+// app.use(express.json());
+// require("../src/database/connection.js");
 // const userRanking = require('../src/models/users');
-// const router = require('../src/routes/routes');
-// app.use(router);
+//  const router = require('../src/routes/routes');
+//  app.use(router);
 
-app.get('/',(req, res)=>{
-    res.send("Hello world");
-});
+// app.get('/',(req, res)=>{
+//     res.send("Hello world");
+// });
 
-app.listen(port,() =>{
-    console.log(`This port is listen in ${port}`);
-});
+// app.listen(port,() =>{
+//     console.log(`This port is listen in ${port}`);
+// });
 
 
 
